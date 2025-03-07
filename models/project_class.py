d3d0716ecfe84babac442861d9b35b2a2424e7be
Mantas <ekspertai08@gmail.com>
2025-03-07 11:43:59 +0200

    Added project class

---
+++ models/project_class.py
@@ -0,0 +1,20 @@
+import models.DB_class as db
+from sqlalchemy import Column, Integer, String, Date, ForeignKey
+
+class Project(db.Base):
+    __tablename__ = 'projects'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False)
+    description = Column(String(100), nullable=False)
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+    
+    def __str__(self):
+        return f"{self.id} | {self.name} | {self.description}"
+    
+    def __repr__(self):
+        return f"{self.name}"
+        
+db.Base.metadata.create_all(db.engine)
